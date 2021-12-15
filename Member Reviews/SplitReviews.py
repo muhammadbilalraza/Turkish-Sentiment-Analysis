f86bfa996c9e2627aaa010de3cdc2fd02ce8ce1e
Muhammad Bilal Raza <muhammadbilalraza@outlook.com>
2021-12-15 05:07:09 +0300

    init

---
+++ Member Reviews/SplitReviews.py
@@ -0,0 +1,31 @@
+from openpyxl import load_workbook
+from Corpus.TurkishSplitter import TurkishSplitter
+
+wb = load_workbook('Member Reviews.xlsx')
+ws = wb.active
+
+print('Counting number of movies...')
+
+moviesCount = 1
+for row in ws:
+  moviesCount += 1
+
+print(f'Total movies: {moviesCount}')
+
+
+print('Starting Split...')
+
+ts = TurkishSplitter()
+
+for movie in range(2, moviesCount):
+  print('--------------------------')
+  print(f'Opening {movie-1} file...')
+  file = open(f'{movie-1}.txt', 'a', encoding='utf-8')  
+  review = ws[f'D{movie}'].value
+
+  print(f'Writing {movie-1} review...')
+  for sentence in ts.split(review):
+    file.write(str(sentence) + '\n')
+  print(f'Closing {movie-1} review...')
+  file.close
+  print('--------------------------')
