eeda6bd80dcc9b4bb4c42d78c908469870771852
muhammadbilalraza <muhammadbilalraza@outlook.com>
2021-12-23 13:57:16 +0300

    data cleaning

---
+++ Correct Misspellings/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String misSpellingsFile = "turkish_misspellings.txt";
+        String bpReviewDict = "bp_reviews_unique_dict.txt";
+        String mReviewDict = "member_reviews_unique_dict.txt";
+
+
+//        ConvertToDictionary dicCon = new ConvertToDictionary();
+//        dicCon.makeDictionary(misSpellingsFile, bpReviewDict, bpReviewDict.replaceAll(".txt", "_dictionary.txt"));
+//        dicCon.makeDictionary(misSpellingsFile, mReviewDict, mReviewDict.replaceAll(".txt", "_dictionary.txt"));
+//
+//        WriteReviews t = new WriteReviews();
+//        t.correctFile();
+
+    }
+
+}
